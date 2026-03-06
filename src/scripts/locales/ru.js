6ecefb0ec2546de6a56f6e2ee8e10d84d7901385
l1nky358 <prokotrob@gmail.com>
2026-03-06 19:01:05 +0700

    Update ru.js

--- src/scripts/locales/ru.js
+++ src/scripts/locales/ru.js
@@ -1,33 +1,36 @@
 export default {
-  translation: {
-    app: {
-      title: 'RSS агрегатор',
-      description: 'Начните читать RSS сегодня! Это легко, красиво и удобно.',
-    },
-    form: {
-      label: 'Ссылка RSS',
-      button: 'Добавить',
-      feedback: {
+  ru: {
+    translation: {
+      app: {
+        title: 'RSS агрегатор',
+        description: 'Начните читать RSS сегодня! Это легко, красиво и удобно.',
+      },
+      form: {
+        label: 'Ссылка RSS',
+        button: 'Добавить',
         loading: 'Загрузка...',
         success: 'RSS успешно загружен',
+      },
+      errors: {
+        required: 'Не должно быть пустым',
+        invalidUrl: 'Ссылка должна быть валидным URL',
+        duplicate: 'RSS уже существует',
         network: 'Ошибка сети',
         noRss: 'Ресурс не содержит валидный RSS',
-        invalidUrl: 'Ссылка должна быть валидным URL',
-        exists: 'RSS уже существует',
       },
-    },
-    feeds: {
-      title: 'Фиды',
-      empty: 'Фиды отсутствуют',
-    },
-    posts: {
-      title: 'Посты',
-      empty: 'Посты отсутствуют',
-      button: 'Просмотр',
-    },
-    modal: {
-      close: 'Закрыть',
-      readFull: 'Читать полностью',
+      feeds: {
+        title: 'Фиды',
+        empty: 'Фиды отсутствуют',
+      },
+      posts: {
+        title: 'Посты',
+        empty: 'Посты отсутствуют',
+        button: 'Просмотр',
+      },
+      modal: {
+        close: 'Закрыть',
+        readFull: 'Читать полностью',
+      },
     },
   },
 };
