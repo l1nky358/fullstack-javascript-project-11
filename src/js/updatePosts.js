d0fe36e0c41b480eed780eecb2ea39ef314ee5ed
l1nky358 <prokotrob@gmail.com>
2026-03-07 16:32:47 +0700

    Update updatePosts.js

--- src/js/updatePosts.js
+++ src/js/updatePosts.js
@@ -1,40 +1,43 @@
-import fetchRss from './httpClient.js';
-import parseRss from './rssParser.js';
+import fetchRss from './httpClient.js'
+import parseRss from './rssParser.js'
+
+let idCounter = 0
+const generateId = () => {
+  idCounter += 1
+  return idCounter
+}
 
 const updatePosts = (watchedState) => {
   const checkFeed = (feed) => {
     return fetchRss(feed.url)
       .then((xmlString) => parseRss(xmlString, feed.url))
       .then((data) => {
-        const existingPosts = watchedState.posts.filter((p) => p.feedId === feed.id);
-        const existingLinks = existingPosts.map((p) => p.link);
-        
+        const existingPosts = watchedState.posts.filter((p) => p.feedId === feed.id)
+        const existingLinks = existingPosts.map((p) => p.link)
+
         const newPosts = data.posts
           .filter((post) => !existingLinks.includes(post.link))
           .map((post) => ({
             ...post,
             id: generateId(),
             feedId: feed.id,
-          }));
-        
+          }))
+
         if (newPosts.length > 0) {
-          watchedState.posts = [...watchedState.posts, ...newPosts];
+          watchedState.posts = [...watchedState.posts, ...newPosts]
         }
       })
       .catch((err) => {
-        console.error('Ошибка обновления фида', feed.url, err);
-      });
-  };
+        console.error('Ошибка обновления фида', feed.url, err)
+      })
+  }
 
-  const promises = watchedState.feeds.map(checkFeed);
-  
+  const promises = watchedState.feeds.map(checkFeed)
+
   return Promise.all(promises)
     .finally(() => {
-      setTimeout(() => updatePosts(watchedState), 5000);
-    });
-};
+      setTimeout(() => updatePosts(watchedState), 5000)
+    })
+}
 
-const generateId = () => {
-};
-
-export default updatePosts;
+export default updatePosts
