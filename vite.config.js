2803905f555fe69449cfb729b5d6f1c0353be56e
l1nky358 <prokotrob@gmail.com>
2026-03-07 14:50:38 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,18 +2,24 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
-  root: process.env.NODE_ENV === 'production' ? 'src' : '.',
+  root: '.',
   build: {
     outDir: 'dist',
     emptyOutDir: true,
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'src/index.html'),
+        main: resolve(__dirname, 'index.html'),
       },
     },
   },
   server: {
     open: true,
     port: 3000,
+  },
+  // Указываем, где искать исходники
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
   },
 });
