0fdcb0dd9b43910bac89c05f706b4d5da35b331f
l1nky358 <prokotrob@gmail.com>
2026-03-07 16:10:09 +0700

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -25,54 +25,44 @@ const generateId = () => {
   return idCounter
 }
 
-const addFeed = (url, watchedState) => {
-  return fetchRss(url)
-    .then((xmlString) => parseRss(xmlString, url))
-    .then((data) => {
-      const feedId = generateId()
-      
-      const newFeed = { ...data.feed, id: feedId, url }
-      watchedState.feeds.push(newFeed)
-      
-      const newPosts = data.posts.map((post) => ({
-        ...post,
-        id: generateId(),
-        feedId,
-      }))
-      
-      watchedState.posts = [...watchedState.posts, ...newPosts]
-      
-      return data
-    })
-    .catch((err) => {
-      if (err.message === 'errors.invalidRss') {
-        throw new Error(i18next.t('errors.invalidRss'))
-      }
-      throw new Error(i18next.t('errors.network'))
-    })
-}
+const addFeed = (url, watchedState) => fetchRss(url)
+  .then(xmlString => parseRss(xmlString, url))
+  .then(data => {
+    const feedId = generateId()
+    const newFeed = { ...data.feed, id: feedId, url }
+    watchedState.feeds.push(newFeed)
+    const newPosts = data.posts.map(post => ({
+      ...post,
+      id: generateId(),
+      feedId,
+    }))
+    watchedState.posts = [...watchedState.posts, ...newPosts]
+    return data
+  })
+  .catch(err => {
+    if (err.message === 'errors.invalidRss') {
+      throw new Error(i18next.t('errors.invalidRss'))
+    }
+    throw new Error(i18next.t('errors.network'))
+  })
 
 const app = () => {
   const watchedState = initView(state)
-  
   const form = document.querySelector('.rss-form')
-  form.addEventListener('submit', (e) => {
+  form.addEventListener('submit', e => {
     e.preventDefault()
     const formData = new FormData(e.target)
     const url = formData.get('url')
-    
-    const existingUrls = watchedState.feeds.map((feed) => feed.url)
-    
+    const existingUrls = watchedState.feeds.map(feed => feed.url)
     watchedState.form.status = 'sending'
     watchedState.form.valid = true
     watchedState.form.error = null
-    
     validate(url, existingUrls)
       .then(() => addFeed(url, watchedState))
       .then(() => {
         watchedState.form.status = 'finished'
       })
-      .catch((err) => {
+      .catch(err => {
         watchedState.form.valid = false
         watchedState.form.error = err.message
         watchedState.form.status = 'failed'
