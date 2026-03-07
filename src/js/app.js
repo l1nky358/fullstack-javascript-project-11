4cdd6bd6ee4a0804c2b300d2e604812d0fec4f8f
l1nky358 <prokotrob@gmail.com>
2026-03-07 16:29:27 +0700

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -28,7 +28,7 @@ const generateId = () => {
 const addFeed = (url, watchedState) => {
   return fetchRss(url)
     .then(xmlString => parseRss(xmlString, url))
-    .then(data) => {
+    .then(data => {
       const feedId = generateId()
       const newFeed = { ...data.feed, id: feedId, url }
       watchedState.feeds.push(newFeed)
@@ -40,7 +40,7 @@ const addFeed = (url, watchedState) => {
       watchedState.posts = [...watchedState.posts, ...newPosts]
       return data
     })
-    .catch(err) => {
+    .catch(err => {
       if (err.message === 'errors.invalidRss') {
         throw new Error(i18next.t('errors.invalidRss'))
       }
@@ -51,7 +51,7 @@ const addFeed = (url, watchedState) => {
 const app = () => {
   const watchedState = initView(state)
   const form = document.querySelector('.rss-form')
-  form.addEventListener('submit', e) => {
+  form.addEventListener('submit', e => {
     e.preventDefault()
     const formData = new FormData(e.target)
     const url = formData.get('url')
@@ -64,7 +64,7 @@ const app = () => {
       .then(() => {
         watchedState.form.status = 'finished'
       })
-      .catch(err) => {
+      .catch(err => {
         watchedState.form.valid = false
         watchedState.form.error = err.message
         watchedState.form.status = 'failed'
