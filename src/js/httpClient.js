de1812f96e018be0e1f333b4fc4944628faf0310
l1nky358 <prokotrob@gmail.com>
2026-03-07 15:16:33 +0700

    Update httpClient.js

--- src/js/httpClient.js
+++ src/js/httpClient.js
@@ -1,15 +1,14 @@
 import axios from 'axios';
 
-const proxyUrl = 'https://allorigins.hexlet.app/get';
-
 const fetchRss = (url) => {
+  const proxyUrl = 'https://allorigins.hexlet.app/get';
   const requestUrl = new URL(proxyUrl);
   requestUrl.searchParams.set('disableCache', 'true');
   requestUrl.searchParams.set('url', url);
 
   return axios.get(requestUrl.toString())
     .then((response) => {
-      if (response.data?.contents) {
+      if (response.data && response.data.contents) {
         return response.data.contents;
       }
       throw new Error('Invalid response from proxy');
