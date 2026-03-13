10701bd4d5a481f53e425e1f28c5ce8680c35236
l1nky358 <prokotrob@gmail.com>
2026-03-13 14:37:04 +0700

    Update rssParser.js

--- src/js/rssParser.js
+++ src/js/rssParser.js
@@ -7,14 +7,17 @@ const parseRss = (xmlString) => {
     error.isParsingError = true
     throw error
   }
+  
   const feedTitle = xmlDoc.querySelector('channel > title')?.textContent || 'Без названия'
   const feedDescription = xmlDoc.querySelector('channel > description')?.textContent || 'Без описания'
+  
   const items = xmlDoc.querySelectorAll('item')
   const posts = Array.from(items).map(item => ({
     title: item.querySelector('title')?.textContent || 'Без названия',
     description: item.querySelector('description')?.textContent || 'Без описания',
     link: item.querySelector('link')?.textContent || '#',
   }))
+  
   return {
     feed: {
       title: feedTitle,
