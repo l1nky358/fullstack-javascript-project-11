e1a7fe3622097982c415bc51ad40d76e3b7539bc
l1nky358 <prokotrob@gmail.com>
2026-03-13 14:45:08 +0700

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -25,31 +25,33 @@ const generateId = () => {
   return idCounter
 }
 
-const addFeed = (url, watchedState) => fetchRss(url)
-  .then(xmlString => parseRss(xmlString))
-  .then(data) => {
-    const feedId = generateId()
-    const newFeed = { ...data.feed, id: feedId, url }
-    watchedState.feeds.push(newFeed)
-    const newPosts = data.posts.map(post => ({
-      ...post,
-      id: generateId(),
-      feedId,
-    }))
-    watchedState.posts = [...watchedState.posts, ...newPosts]
-    return data
-  })
-  .catch(err) => {
-    if (err.message === 'errors.invalidRss') {
-      throw new Error(i18next.t('errors.invalidRss'))
-    }
-    throw new Error(i18next.t('errors.network'))
-  })
+const addFeed = (url, watchedState) => {
+  return fetchRss(url)
+    .then(xmlString => parseRss(xmlString))
+    .then(data => {
+      const feedId = generateId()
+      const newFeed = { ...data.feed, id: feedId, url }
+      watchedState.feeds.push(newFeed)
+      const newPosts = data.posts.map(post => ({
+        ...post,
+        id: generateId(),
+        feedId,
+      }))
+      watchedState.posts = [...watchedState.posts, ...newPosts]
+      return data
+    })
+    .catch(err => {
+      if (err.message === 'errors.invalidRss') {
+        throw new Error(i18next.t('errors.invalidRss'))
+      }
+      throw new Error(i18next.t('errors.network'))
+    })
+}
 
 const app = () => {
   const watchedState = initView(state)
   const form = document.querySelector('.rss-form')
-  form.addEventListener('submit', e) => {
+  form.addEventListener('submit', e => {
     e.preventDefault()
     const formData = new FormData(e.target)
     const url = formData.get('url')
@@ -62,7 +64,7 @@ const app = () => {
       .then(() => {
         watchedState.form.status = 'finished'
       })
-      .catch(err) => {
+      .catch(err => {
         watchedState.form.valid = false
         watchedState.form.error = err.message
         watchedState.form.status = 'failed'
