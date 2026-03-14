f7fa689fdf2ff3560e92ccd7821554b3f04dcee2
l1nky358 <prokotrob@gmail.com>
2026-03-14 14:42:02 +0700

    Update updatePosts.js

--- src/js/updatePosts.js
+++ src/js/updatePosts.js
@@ -2,14 +2,14 @@ import fetchRss from './httpClient.js'
 import parseRss from './rssParser.js'
 
 const updatePosts = (state) => {
-  const checkFeed = feed => fetchRss(feed.url)
-    .then(xmlString => parseRss(xmlString))
-    .then(data => {
-      const existingPosts = state.posts.filter(p => p.feedId === feed.id)
-      const existingLinks = existingPosts.map(p => p.link)
+  const checkFeed = (feed) => fetchRss(feed.url)
+    .then((xmlString) => parseRss(xmlString))
+    .then((data) => {
+      const existingPosts = state.posts.filter((p) => p.feedId === feed.id)
+      const existingLinks = existingPosts.map((p) => p.link)
       const newPosts = data.posts
-        .filter(post => !existingLinks.includes(post.link))
-        .map(post => ({
+        .filter((post) => !existingLinks.includes(post.link))
+        .map((post) => ({
           ...post,
           id: Date.now() + Math.random(),
           feedId: feed.id,
@@ -18,7 +18,7 @@ const updatePosts = (state) => {
         state.posts = [...state.posts, ...newPosts]
       }
     })
-    .catch(err => {
+    .catch((err) => {
       console.error('Update error:', err)
     })
 
