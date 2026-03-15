8eb8f592ffa0d57c1a0204f49094c1651f4a6d33
l1nky358 <prokotrob@gmail.com>
2026-03-15 12:12:23 +0700

    Update validate.js

--- src/js/validate.js
+++ src/js/validate.js
@@ -3,21 +3,24 @@ import i18next from './locales.js'
 
 yup.setLocale({
   mixed: {
-    required: () => ({ key: 'errors.required' }),
-    notOneOf: () => ({ key: 'errors.alreadyExists' }),
+    required: () => i18next.t('errors.required'),
+    notOneOf: () => i18next.t('errors.alreadyExists'),
   },
   string: {
-    url: () => ({ key: 'errors.invalidUrl' }),
+    url: () => i18next.t('errors.invalidUrl'),
   },
 })
 
 const validate = (url, existingUrls) => {
-  const schema = yup.string().required().url().notOneOf(existingUrls)
+  const schema = yup.string()
+    .required()
+    .url()
+    .notOneOf(existingUrls)
+  
   return schema.validate(url)
     .then(() => null)
     .catch((err) => {
-      const messageKey = err.errors?.[0]?.key || 'errors.invalidUrl'
-      return Promise.reject(new Error(i18next.t(messageKey)))
+      return Promise.reject(new Error(err.message))
     })
 }
 
